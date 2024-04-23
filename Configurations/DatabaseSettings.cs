7bcb6bf56a25a3e80f5711efe130dda58278ab40
atomicjed <160503304+atomicjed@users.noreply.github.com>
2024-04-23 15:17:20 +0100

    repo reset

--- Configurations/DatabaseSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace Synergy.Configurations;
-
-public class DatabaseSettings
-{
-    public string ConnectionString { get; set; } = null!;
-    public string DatabaseName { get; set; } = null!;
-    public string UsersCollectionName { get; set; } = null!;
-}
