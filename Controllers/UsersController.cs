ab5d4d1d21bedceee87d2982b96191d71f8fd579
jedryan <jed.ryan@atomicmedia.co.uk>
2024-04-23 14:37:26 +0100

    set up commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Synergy.Models;
+using Synergy.Services;
+
+namespace Synergy.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+[Authorize]
+public class UsersController : ControllerBase
+{
+    private readonly UsersService _usersService;
+
+    public UsersController(UsersService usersService)
+    {
+        _usersService = usersService;
+    }
+    
+    [HttpPost("add-user")]
+    public async Task<IActionResult> AddUser([FromBody] Users newUser)
+    {
+        try
+        {
+            await _usersService.AddUser(newUser);
+            return Ok();
+        }
+        catch 
+        {
+            return StatusCode(500, $"Internal server error when adding user");
+        }
+    }
+
+    [HttpPost("get-user")]
+    public async Task<IActionResult> GetUser([FromBody] string userId)
+    {
+        try
+        {
+            var user = await _usersService.GetUser(userId);
+            return Ok(user);
+        }
+        catch
+        {
+            return StatusCode(500, "Error getting user");
+        }
+    }
+}
